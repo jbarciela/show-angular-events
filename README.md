df51269ccc36d7cb5fe990605186481bb5683d62
Jaime Barciela <Jaime.Barciela@blackboard.com>
2016-04-27 16:35:49 -0400

    add todo items

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # show-angular-events
 A tool to discover events and dependencies in an Angular application.
+
+
+##### TODO
+
+- Run against opensource angular apps
+- Serve a dynamic web iface (Scala js)
--- src/main/scala/com/jbarciela/showangularevents/Hello.scala
+++ src/main/scala/com/jbarciela/showangularevents/Hello.scala
@@ -3,6 +3,14 @@ package com.jbarciela.showangularevents
 import java.io.File;
 import ammonite.ops._
 
+/*
+ * TODO:
+ * - unique list of event names
+ * - association with file name
+ * - add direction to sources
+ * -- $emit sends messages up the scope chain
+ * -- $broadcast sends messages down 
+ */
 object Hello {
   
   val extensions: List[String] = List(".js", ".ts")
